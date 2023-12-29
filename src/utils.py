90f51b4da58aff6e703ef410135d1c8107a48589
Alina Salakhova <alina.salakhova1991@gmail.com>
2023-12-29 01:31:36 +0100

    Добавлена функция для получения последних пяти выполненных транзакций

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,13 @@ def sort_transactions_by_date(transaction):
     Возвращает дату операции для сортировки.
     """
     return datetime.strptime(transaction['date'], '%Y-%m-%dT%H:%M:%S.%f')
+
+
+
+def get_last_five_executed(transactions):
+    """
+    Возвращает последние пять выполненных операций.
+    """
+    executed_transactions = [t for t in transactions if t.get('state') == 'EXECUTED']
+    executed_transactions.sort(key=sort_transactions_by_date, reverse=True)
+    return executed_transactions[:5]
