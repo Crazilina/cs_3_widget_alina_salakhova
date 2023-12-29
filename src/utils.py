425b9386a1b7307b99c92f9a63f740a1a00f5bf2
Alina Salakhova <alina.salakhova1991@gmail.com>
2023-12-29 01:28:23 +0100

    Добавлена функция для сортировки транзакций по дате

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+
 
 def load_transactions(filename):
     """
@@ -6,3 +8,10 @@ def load_transactions(filename):
     """
     with open(filename, 'r') as file:
         return json.load(file)
+
+
+def sort_transactions_by_date(transaction):
+    """
+    Возвращает дату операции для сортировки.
+    """
+    return datetime.strptime(transaction['date'], '%Y-%m-%dT%H:%M:%S.%f')
