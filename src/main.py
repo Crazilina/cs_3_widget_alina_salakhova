78d81a18b18eacd7de93595dc1ddab02fc3b55d2
Alina Salakhova <alina.salakhova1991@gmail.com>
2023-12-30 01:21:21 +0100

    Добавлен абсолютный путь к файлу JSON

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import os
 from utils import load_transactions, get_last_five_executed, display_transaction
 
 
 def main():
-    transactions = load_transactions('operations.json')
+    # Создаем абсолютный путь к файлу operations.json
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    json_file_path = os.path.join(current_dir, "operations.json")
+
+    transactions = load_transactions(json_file_path)
     last_five_transactions = get_last_five_executed(transactions)
     for transaction in last_five_transactions:
         output = display_transaction(transaction)
